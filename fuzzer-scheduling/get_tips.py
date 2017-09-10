53e78d26409808679ee9d9ebc6793ca8e989a80a
yuguorui <yuguorui96@gmail.com>
2017-09-10 15:31:09 +0800

    整合生成自定义生成测试用例的模块。

---
+++ fuzzer-scheduling/get_tips.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import sys
+import os.path
+import hashlib
+import signal
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+import testcase_gen
+
+class TimeoutException(Exception):
+    pass
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutException(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
+def tips_path(path):
+    tmp_path = os.path.join("/tmp", hashlib.sha256(path).hexdigest())
+    try:
+        with timeout(seconds=10):
+            if not os.path.exists(tmp_path):
+                os.makedirs(tmp_path)
+            g = testcase_gen.tc_gen(path)
+            g.run()
+            with open(os.path.join(tmp_path, "testcase.txt"), "wb") as f:
+                f.write(g.get_testcase())
+            print("[ ] testcase-gen success.")
+    except TimeoutException as e:
+        print("[!] testcase-gen timeout!")
+        return None
+    except RuntimeError:
+        print("[!] testcase-gen error!")
+        return None
+
+    return tmp_path
