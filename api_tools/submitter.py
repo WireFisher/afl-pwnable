a071032ff8885cc0ed07d2cb38f807869c06a4ed
Harry Chen <i@harrychen.xyz>
2017-09-10 17:22:13 +0800

    Handle multiply payload files in submitter
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- api_tools/submitter.py
+++ api_tools/submitter.py
@@ -5,6 +5,7 @@ import json
 import os
 import time
 import nclib
+import glob
 
 
 def submit_to_server(server, payload, token):
@@ -32,25 +33,30 @@ def submit_to_server(server, payload, token):
         return False
 
 
-def submit_challenge(token, location):
+def submit_challenge(token, d):
     print('======================')
     print('Found challenge', d)
-    if os.path.exists(d + '/payload.bin'):
-        print('Payload found')
-        if os.path.exists(d + '/success.flag'):
+    payload_files = glob.glob(d + '/payload.bin.*')
+
+    for payload_file in payload_files:
+        if 'success' in payload_file:  # success flag
+            continue
+
+        print('Payload found:' + payload_file)
+        if os.path.exists(payload_file + '.success'):
             print('Already successfully solved, skipping to next')
-            return
+            continue
         else:
             with open(d + '/metadata.json') as meta_file:
-                payload = open(d + '/payload.bin', 'rb').read()
+                payload = open(payload_file, 'rb').read()
                 metadata = json.load(meta_file)
                 access = metadata['access']
                 for server in access:
                     if submit_to_server(server, payload, token):
-                        with open(d + '/success.flag', 'w') as flag:
+                        with open(payload_file + '.success', 'w') as flag:
                             flag.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
                         print('Successfully solved!')
-                        return
+                        return True
                 print('All attempts failed')
     else:
         print('Payload not found, skipping to next')
